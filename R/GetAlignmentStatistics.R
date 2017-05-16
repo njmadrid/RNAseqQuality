5da162a1e3551736bddfbd26cdfe5bb727651409
njmadrid <njmadrid@salud.unm.edu>
2017-05-16 19:35:06 +0000

    v4

--- R/GetAlignmentStatistics.R
+++ R/GetAlignmentStatistics.R
@@ -10,18 +10,19 @@
 #' AlignmentStatsDF = GetAlignmentStatistics(BamFilePaths[1])
 #' @export
 GetAlignmentStatistics <- function(BamFilePath) {
-  df <- data.frame(strings=system(paste("samtools idxstats ", BamFilePath), intern=T))
+  TextDF <- data.frame(strings=system(paste("samtools idxstats ", BamFilePath), intern=T))
+  TotalReads <- tidyr::separate(TextDF, strings, into=c("gene", "length", "counts", "unaligned"), sep = "\t")
+  TotalReads <- plyr::summarise(TotalReads, "Total Reads"=sum(as.integer(counts))+sum(as.integer(unaligned)))
   
-  TotalReads <- df %>% tidyr::separate(strings, into=c("gene","length","counts","unaligned"),sep = "\t") %>% 
-    plyr::summarise("Total Reads"=sum(as.integer(counts))+sum(as.integer(unaligned)))
+  TextDF <- data.frame(strings=system(paste("samtools idxstats ", BamFilePath), intern=T))
+  TotalAligned <- tidyr::separate(TextDF, strings, into=c("gene", "length", "counts", "unaligned"), sep = "\t")
+  TotalAligned <- plyr::summarise(TotalAligned, "Total Mapped Read Count"=sum(as.integer(counts)))
   
-  TotalAligned <- df %>% tidyr::separate(strings, into=c("gene","length","counts","unaligned"),sep = "\t") %>% 
-    plyr::summarise("Total Mapped Read Count"=sum(as.integer(counts)))
+  TextDF <- data.frame(strings=system(paste("samtools idxstats ", BamFilePath), intern=T))
+  TotalUnaligned <- tidyr::separate(TextDF, strings, into=c("gene", "length", "counts", "unaligned"), sep = "\t")
+  TotalUnaligned <- plyr::summarise(TotalUnaligned, "Total Unmapped Read Count"=sum(as.integer(unaligned)))
   
-  TotalUnaligned <- df %>% tidyr::separate(strings, into=c("gene","length","counts","unaligned"),sep = "\t") %>% 
-    plyr::summarise("Total Unmapped Read Count"=sum(as.integer(unaligned)))
-  
-  out <- data.frame(TotalReads, TotalAligned, TotalUnaligned, "Percent.Mapped" = TotalAligned [[1]] / TotalReads[[1]])
+  out <- data.frame(TotalReads, TotalAligned, TotalUnaligned, "Percent.Mapped" = TotalAligned[[1]] / TotalReads[[1]] * 100)
   rownames(out) <- basename(BamFilePath)
   return(out)
 }
